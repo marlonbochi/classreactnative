4a05ac93f5d3baa13e0eab2fff9c4e1c2a315cf5
Marlon Bochi <marlon.bueno@inevent.us>
2019-03-19 18:07:58 -0300

    Set style of list.

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 
 // services
 import api from "../services/api";
@@ -27,23 +27,75 @@ export default class Main extends Component {
     }
 
     renderItem = ({ item }) => (
-        <View>
-            <Text>{ item.title }</Text>
-            <Text>{ item.description }</Text>
-            <TouchableOpacity onPress={ () => {} }>
-                <Text>Acessar</Text>
+        <View style={styles.productContainer}>
+            <Text style={styles.productTitle}>{ item.title }</Text>
+            <Text style={styles.productDescription}>{ item.description }</Text>
+
+            <TouchableOpacity style={styles.productButton} onPress={ () => {} }>
+                <Text style={styles.productButtonText}>Acessar</Text>
             </TouchableOpacity>
         </View>
     );
     
     render(){
         return(
-            <FlatList 
-            data = { this.state.docs }
-            keyExtractor = { item => item._id }
-            renderItem = { this.renderItem }
-            >
-            </FlatList>
+            <View style={styles.container}>
+                <FlatList 
+                contentContainerStyle={styles.list }
+                data = { this.state.docs }
+                keyExtractor = { item => item._id }
+                renderItem = { this.renderItem }
+                >
+                </FlatList>
+            </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FAFAFA"
+    },
+
+    list: {
+        padding: 20
+    },
+
+    productContainer: {
+        backgroundColor: "#FFF",
+        borderWidth: 1,
+        borderColor: "#DDD",
+        borderRadius: 5, 
+        padding: 20,
+        marginBottom: 20
+    },
+
+    productTitle:{
+        fontSize: 18,
+        fontWeight: "bold" ,
+        color: "#333"
+    },
+    productDescription: {
+        color: "#999",
+        fontSize: 16,
+        marginTop: 5,
+        lineHeight: 24
+    },
+
+    productButton: {
+        height: 42,
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: "#DA552F",
+        backgroundColor: "transparent",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 10,
+    },
+
+    productButtonText: {
+        fontSize: 16,
+        color: "#DA552F"
+    }
+});
